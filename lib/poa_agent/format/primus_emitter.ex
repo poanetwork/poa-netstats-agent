bed2126bba0f5867aee37d5f8a3dd3d64005e2e5
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2018-05-08 16:35:14 +0200

    Include identifier & optional secret when building Primus-like events
    The node identifier is sent over in every Primus-like event that the
    server expects.

--- lib/poa_agent/format/primus_emitter.ex
+++ lib/poa_agent/format/primus_emitter.ex
@@ -1,7 +1,13 @@
 defmodule POAAgent.Format.PrimusEmitter do
   @moduledoc false
 
-  def write(event: e, data: d) do
-    %{emit: [e, d]}
+  def write(id: i, event: e, name: n, data: d) do
+    value = %{"id" => i, n => d}
+    %{emit: [e, value]}
+  end
+
+  def write(id: i, event: e, name: n, data: d, secret: s) do
+    value = %{"id" => i, n => d, "secret" => s}
+    %{emit: [e, value]}
   end
 end
