afbb24e22df2d5d4143fe97031424779d06291a5
Felipe Ripoll <ferigis@gmail.com>
2018-05-15 08:42:35 -0600

    [#19] filling the Information struct before starting the WS connection

--- config/test.exs
+++ config/test.exs
@@ -4,4 +4,8 @@ config :poa_agent, POAAgent.Plugins.Transfers.WebSocket.Primus,
     address: "ws://localhost:3000/api",
     identifier: "elixirNodeJSIntegration",
     name: "Elixir-NodeJS-Integration",
-    secret: ""
+    secret: "",
+    contact: "mymail@mail.com"
+
+config :ethereumex,
+    url: "http://localhost:8545"
