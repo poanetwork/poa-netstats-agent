efe5a71d5006a68059f68f8d269c0471e3e9211d
Felipe Ripoll <ferigis@gmail.com>
2018-05-17 09:40:06 -0600

    [#30] adding a full test for Pending Collector

--- test/poa_agent/plugins/collectors/eth/pending_test.exs
+++ test/poa_agent/plugins/collectors/eth/pending_test.exs
@@ -1,20 +1,51 @@
 defmodule POAAgent.Plugins.Collectors.Eth.PendingTest do
   use ExUnit.Case
+
+  alias POAAgent.Plugins.Collectors.Eth.Pending
+
   import Mock
 
-  test "pending data sent to the transfer" do
+  test "pending data is not sent if it is the same as last pending" do
+    echo_transfer = :echo_transfer
+    {:ok, _echo} = EchoTransfer.start(echo_transfer)
+
+    args = %{
+      name: :eth_pending,
+      transfers: [echo_transfer],
+      frequency: 500,
+      label: :my_metrics,
+      args: [url: "http://localhost:8545"]
+    }
+
     with_mock Ethereumex.HttpClient, [
         eth_get_block_transaction_count_by_number: fn(_) -> {:ok, "0x0"} end
       ] do
 
-      {:notransfer, _} = POAAgent.Plugins.Collectors.Eth.Pending.collect(%{last_pending: 0})
+      {:ok, _pid} = Pending.start_link(args)
+
+      refute_receive {:my_metrics, _}
     end
+  end
+
+  test "pending data is sent if it is different than last pending" do
+    echo_transfer = :echo_transfer
+    {:ok, _echo} = EchoTransfer.start(echo_transfer)
+
+    args = %{
+      name: :eth_pending,
+      transfers: [echo_transfer],
+      frequency: 500,
+      label: :my_metrics,
+      args: [url: "http://localhost:8545"]
+    }
+
     with_mock Ethereumex.HttpClient, [
         eth_get_block_transaction_count_by_number: fn(_) -> {:ok, "0x3"} end
       ] do
 
-      {:transfer, pending, _} = POAAgent.Plugins.Collectors.Eth.Pending.collect(%{last_pending: 0})
-      assert pending == expected_pending()
+      {:ok, _pid} = Pending.start_link(args)
+
+      assert_receive {:my_metrics,  %POAAgent.Entity.Ethereum.Pending{pending: 3}}, 20_000
     end
   end
 
