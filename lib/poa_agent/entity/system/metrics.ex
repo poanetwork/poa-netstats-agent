4eae4c3e4177c85a22a4afabf9f2c96f65449dba
Filipe <varjaofilipe@gmail.com>
2018-07-19 18:15:17 -0300

    missing the asserts

--- lib/poa_agent/entity/system/metrics.ex
+++ lib/poa_agent/entity/system/metrics.ex
@@ -2,10 +2,18 @@ defmodule POAAgent.Entity.System.Metrics do
   @moduledoc false
 
   @type t :: %__MODULE__{
-    os_type: {unix | win32, atom},
-    unix_process: integer | {error, term},
-    cpu_util: float | {error, term},
+    os_type: {atom, atom},
+    unix_process: integer | nil,
+    cpu_util: float | nil,
     disk_used: integer,
     memsup: [{atom, integer}]
   }
+
+  defstruct [
+    os_type: nil,
+    unix_process: nil,
+    cpu_util: nil,
+    disk_used: nil,
+    memsup: nil
+  ]
 end
--- test/poa_agent/plugins/collectors/system/metrics_test.exs
+++ test/poa_agent/plugins/collectors/system/metrics_test.exs
@@ -7,25 +7,35 @@ defmodule POAAgent.Plugins.Collectors.System.MetricsTest do
     echo_transfer = :echo_transfer
     {:ok, _echo} = EchoTransfer.start(echo_transfer)
 
-    args = %{
-      name: :eth_pending,
-      transfers: [echo_transfer],
-      frequency: 500,
-      label: :my_metrics,
-      args: [url: "http://localhost:8545"]
-    }
-    {:ok, _pid} = Metrics.start(echo_transfer)
-    expected_metrics = expected_metrics(args)
-
-    assert_receive {:my_metrics, ^expected_info}, 20_000
+    assert_receive {:my_metrics, %Metrics{os_type: os_type,
+                                          unix_process: nprocs, cpu_util: util,
+                                          disk_used: disksup, memsup: memsup}},
+                                          20_000
+    # assert os_type
+    assert nprocs 
+    # assert util
+    # assert disksup
+    # assert memsup
   end
+  #   args = %{
+  #     name: :eth_pending,
+  #     transfers: [echo_transfer],
+  #     frequency: 500,
+  #     label: :my_metrics,
+  #     args: [url: "http://localhost:8545"]
+  #   }
+  #   {:ok, _pid} = Metrics.start(echo_transfer)
+  #   expected_metrics = expected_metrics(args)
 
-  def expected_metrics() do
-    %Metrics{
-      os_type: {unix | win32, atom},
-      unix_process: integer | {error, term},
-      cpu_util: float | {error, term},
-      disk_used: integer,
-      memsup: [{atom, integer}]
-  end
+  #   assert_receive {:my_metrics, ^expected_metrics}, 20_000
+  # end
+
+  # def expected_metrics() do
+  #   %Metrics{
+  #     os_type: {atom, atom},
+  #     unix_process: integer,
+  #     cpu_util: float,
+  #     disk_used: integer,
+  #     memsup: [{atom, integer}]}
+  # end
 end
